987778e76a30ebf93a32b5f26172fce9543cd29f
shailesh <shaileshpinate7@gmail.com>
2024-10-05 16:31:33 +0530

    initial commit

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "child"
+}, [
+    React.createElement("h1", {}, "im h1 tag"),
+    React.createElement("h2", {}, "i'm h2 tag")
+]));
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
